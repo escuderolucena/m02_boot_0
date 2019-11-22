275fe4b3b9684a7870e99d7c0058b838c741d9b1
neowizard2 <esmicer@gmail.com>
2019-11-22 13:02:21 +0100

    map, filter y reduce

--- funcionesLambda.py
+++ funcionesLambda.py
@@ -7,5 +7,6 @@ triple = lambda x: x*3
 print(sumaTodos(3, doble))
 print(sumaTodos(100, triple))
 
+# da igual la forma en la que lo pongas 
 print(sumaTodos(3, lambda x: x*2))
 print(sumaTodos(100, lambda x: x*3))
